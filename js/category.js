dc80fab627bffb62392ceb6f2e547489b5507cfb
biancapazudesa <pazb@udesa.edu.ar>
2025-11-15 01:48:58 -0300

    Segui con el aside, ya se logre que sea dinamico pero todavia no para todas las categorias

--- js/category.js
+++ js/category.js
@@ -1,19 +1,19 @@
-let producto = document.querySelector(".contenedorpadreblush")
-let URL = 'https://dummyjson.com/products'
+let queryString = location.search;
+let query = new URLSearchParams(queryString);
+let categoria = query.get("categoria");
+let productos = document.querySelector(".contenedorpadreblush")
+
+let URL = `https://dummyjson.com/products/category/${categoria}`;
 fetch(URL)
     .then(function (res) {
-        return res.json();
+        return res.json()
     })
     .then(function (data) {
-        let listaProductos = data.products;
-        console.log(listaProductos)
-        console.log(listaProductos.length)
-
-        for (let i = 0; i < listaProductos.length; i++) {
+        console.log(data)
+        let listaProductos = data.products
+        for (let i= 0; i < listaProductos.length; i++) {
             const element = listaProductos[i];
-
-
-            producto.innerHTML += `<article class="cadablush">
+            productos.innerHTML += `<article class="cadablush">
                  <a href="./producto.html?id=${element.id}"><img class="imagenblush" src="${element.images[0]}" alt="Blush Happy">
                     <h4 class="nombreblush">${element.title}</h4>
                     <p class="detallesblush"></p>
@@ -21,14 +21,13 @@ fetch(URL)
                 <p class="precio">${element.price} </p>
                 <a class="botoncompra" href="./producto.html?id=${element.id}">Ver más</a>
             </article>`
+
         }
-
-
+        
     })
     .catch(function (err) {
-        console.log(err);
+        console.log(err)
     })
-
 
 
 
