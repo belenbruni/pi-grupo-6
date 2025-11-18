e8970808ba8cfeb58fe511108e59ded90fd92595
biancapazudesa <pazb@udesa.edu.ar>
2025-11-18 00:26:32 -0300

    Hice la parte de resenas dinamicas en el detalle de los productos, tambien perfeccione el fetch de porducto porque aparecia undefined, modifique el CSS de producto y su HTML para que se adapten a lo dinamico.

--- js/producto.js
+++ js/producto.js
@@ -1,12 +1,8 @@
 let queryString = location.search;
 let queryStringObj = new URLSearchParams(queryString);
 let id = queryStringObj.get('id')
-
-let titulo = document.querySelector(".tituloproducto")
-let imagen = document.querySelector(".imagenproducto")
-let categoria = document.querySelector(".detallesproducto2")
-let precio = document.querySelector(".precioDetalle")
-let informacion = document.querySelector(".tonosproducto")
+let contenedorPro = document.querySelector(".contenedorpadreproducto")
+let contenedor = document.querySelector(".reviews")
 
 
 let URL = `https://dummyjson.com/products/${id}`
@@ -17,24 +13,41 @@ fetch(URL)
 })
 .then(function(data){
     console.log(data)
-    titulo.innerText = data.title
-    imagen.src = data.images[0]
-    categoria.innerText = `Categoria: ${data.category}`
-    precio.innerText = data.price
-    informacion.innerHTML = `<li>Marca: ${data.brand}</li>
-                            <li>Stock: ${data.stock}</li> 
-                            <li>Rating: ${data.rating}</li>
-                            <li>Descuento: ${data.discountPercentage}</li>
-                            <li>Garantia: ${data.warrantyInformation}</li>
-                            <li>Estado: ${data.availabilityStatus}</li>
-                            <li>Devolucion: ${data.returnPolicy}</li>
-                            <li>Minimo de pedidos: ${data.minimumOrderQuantity}</li>`
-                            
+    contenedorPro.innerHTML += `
+    <h1 class="tituloproducto">${data.title}</h1>
+                <article class="articleproducto">
+                    <img class="imagenproducto" src=${data.images[0]}>
+                    <p class="detallesproducto2"><a href="./category.html?categoria=${data.category}">Categoria: ${data.category}</a></p>
+                </article class="productodescr">
 
-
-
-    
-    
+                <article class="descriproducto">
+                    <p class="descripcion">${data.description}</p>
+                    <span>Información:</span>
+                    <ul class="tonosproducto">
+                        <li>Marca: ${data.brand}</li>
+                        <li>Stock: ${data.stock}</li> 
+                        <li>Rating: ${data.rating}</li>
+                        <li>Descuento: ${data.discountPercentage}</li>
+                        <li>Garantía: ${data.warrantyInformation}</li>
+                        <li>Estado: ${data.availabilityStatus}</li>
+                        <li>Devolución: ${data.returnPolicy}</li>
+                        <li>Mínimo de pedidos: ${data.minimumOrderQuantity}</li>
+                        <li>Tags: ${data.tags}</li>
+                    </ul>
+                    <p class="precioDetalle">$${data.price}</p>
+                    <a class="botoncomprarya" href="./resultados_busqueda.html">Comprar ya!</a>
+                </article>`
+    let reviews = data.reviews
+    for (let i = 0; i < reviews.length; i++) {
+        const element = reviews[i];
+        contenedor.innerHTML +=  `<article class="articleReview">
+                                    <h3 class="nombrereview"> ${element.reviewerName} </h3>
+                                    <h4 class="emailReview"> - ${element.reviewerEmail} -</h4>
+                                    <p class="rating"> Rating: ${element.rating}</p>
+                                    <p class="review">Comentario: ${element.comment}</p>
+                                    <p class="date"> ${element.date}</p>
+                                </article>`
+    }           
 })
 .catch(function(err){
     console.log(err)
